3b730b52e52957d4dec5726d18e09998c5050331
sandeep-wf <sandeep.p@wakefit.co>
2026-06-18 16:38:32 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,2 @@
-const CACHE_NAME = 'wakefit-v1';
-const ASSETS = ['/'];
-
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
-});
-
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
-});
+// Simple Service Worker
+self.addEventListener("fetch", function(event) { });
